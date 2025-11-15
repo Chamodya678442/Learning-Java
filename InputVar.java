672e17779c2d6c88073db5d9b82eaff032ce83d5
Chamodya678442 <bethmagejc678@gmail.com>
2025-11-15 16:48:38 +0530

    Geetting Inputs from user and printing them to the console.

---
+++ InputVar.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class InputVar { public static void main (String[] args){
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter your favourite colour : ");
+        String colour = scanner.nextLine();
+        scanner.nextLine(); //without this, problem occur cuz after it get input after integer
+
+        System.out.print("What is you GPA : ");
+        double GPA = scanner.nextDouble();
+
+        System.out.print("Are you student : ");
+        boolean isStudent = scanner.nextBoolean();
+
+        System.out.println( "Hello " + name + "\n" + "You are "+ age + " years old.\n" + "Your GPA is " + GPA + "\n" + "Student status: " + isStudent);
+        scanner.close();
+    }
+    }
