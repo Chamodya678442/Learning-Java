d512f37253ad7a5aa9467c9425f2ef868f1a912b
Chamodya678442 <bethmagejc678@gmail.com>
2025-11-15 16:07:39 +0530

    Identified basic variable types in java

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+
+    public static void main(String[] args){
+        int age = 22;
+        double number = 13.456;
+        boolean isStudent = true;
+        char letter = 'J';
+        String name = "janodi";
+        System.out.println(age);
+        System.out.println(number);
+        System.out.println(isStudent);
+        System.out.println(letter);
+        System.out.println(name);
+
+    }
+}
